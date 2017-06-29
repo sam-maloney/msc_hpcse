ad95d90aaee81749060b34fc0df0d3be4c3c3aa4
Sam Maloney <56830868+sam-maloney@users.noreply.github.com>
2017-06-29 18:46:13 +0200

    implemented MKL binomial PRNG

--- Project/RW_serial.cpp
+++ Project/RW_serial.cpp
@@ -6,15 +6,12 @@
 #include <vector>
 #include <cmath>
 
+#include "timer.hpp"
 #include "prng_engine.hpp" // Sitmo PRNG
 
 /// Select which runtime measure to use
 //#define USE_TIMER
 #define USE_TSC
-
-#ifdef USE_TIMER
-#include "timer.hpp"
-#endif // USE_TIMER
 
 #ifdef USE_TSC
 #include "tsc_x86.hpp"
@@ -22,6 +19,8 @@
 
 typedef double value_type;
 typedef std::size_t size_type;
+typedef int32_t particle_type;
+typedef uint64_t M_type;
 
 #ifndef M_PI
     constexpr value_type M_PI = 3.14159265358979323846;
@@ -31,7 +30,7 @@ class Diffusion2D {
 public:
     Diffusion2D(const value_type D,
                 const size_type N,
-                const size_type M,
+                const M_type M,
                 const value_type dt)
     : D_(D), N_(N), Ntot(N_*N_), M_(M), dt_(dt)
     {
@@ -124,35 +123,46 @@ public:
 
         value_type ref_value, t_f;
         t_f = time();
-        rms_error_ = 0.0;
 
         for(size_type j = 0; j < N_; ++j) {
-            rms_error_ += pow(rho_[j] - 0, 2);
             out_file << 0.0 << '\t' << (j*dh_) << '\t' << 0.0 << "\n";
         }
 
         for(size_type i = 1; i < N_-1; ++i) {
-            rms_error_ += pow(rho_[i*N_] - 0, 2);
             out_file << (i*dh_) << '\t' << 0.0 << '\t' << 0.0 << "\n";
             for(size_type j = 1; j < N_-1; ++j) {
                 ref_value = sin(M_PI*i*dh_) * sin(M_PI*j*dh_) *
                             exp(-2*D_*t_f*M_PI*M_PI);
-                rms_error_ += pow(rho_[i*N_ + j] - ref_value, 2);
                 out_file << (i*dh_) << '\t' << (j*dh_) << '\t'
                          << ref_value << "\n";
             }
-            rms_error_ += pow(rho_[i*N_ + N_ - 1] - 0, 2);
             out_file << (i*dh_) << '\t' << ((N_-1)*dh_) << '\t' << 0.0 << "\n";
             out_file << "\n";
         }
 
         for(size_type j = 0; j < N_; ++j) {
-            rms_error_ += pow(rho_[(N_-1)*N_ + j] - 0, 2);
             out_file << ((N_-1)*dh_) << '\t' << (j*dh_) << '\t' << 0.0 << "\n";
         }
 
-        rms_error_ = sqrt(rms_error_/(N_*N_));
         out_file.close();
+    }
+
+    value_type compute_rms_error()
+    {
+        rms_error_ = 0.0;
+        value_type t_f = time();
+
+        for(size_type i = 0; i < N_; i++) {
+            for(size_type j = 0; j < N_; j++) {
+                value_type ref_value = sin(M_PI*i*dh_) * sin(M_PI*j*dh_) *
+                                       exp(-2*D_*t_f*M_PI*M_PI);
+                rms_error_ += pow(rho_[i*N_ + j] - ref_value, 2);
+            }
+        }
+
+        rms_error_ = sqrt(rms_error_/(N_*N_));
+
+        return rms_error_;
     }
 
     value_type rms_error() const
@@ -200,66 +210,115 @@ private:
     value_type dh_, dt_, lambda_, fac_, p_stay_, rms_error_;
 
     std::vector<value_type> rho_;
-    std::vector<size_type> m_, m_tmp;
+    std::vector<particle_type> m_, m_tmp;
 
     sitmo::prng_engine eng0;
 };
 
-
 int main(int argc, char* argv[])
 {
-    if (argc < 5) {
-        std::cerr << "Usage: " << argv[0] << " D N M dt (tmax)" << std::endl;
+    timer t_total;
+    t_total.start();
+
+    if (argc < 4) {
+        std::cerr << "Usage: " << argv[0] << " D N M dt (n_runs) (n_steps)" << std::endl;
         return 1;
     }
 
-    const value_type D  = std::stod(argv[1]);
+    const value_type D  = std::stod (argv[1]);
     const size_type  N  = std::stoul(argv[2]);
-    const size_type  M  = std::stoul(argv[3]);
-    const value_type dt = std::stod(argv[4]);
+    const M_type     M  = std::stoul(argv[3]);
+    const value_type dt = std::stod (argv[4]);
 
+//    if ( N % 8 != 0 ) {
+//        N += 8 - (N % 8);
+//        std::cout << "N must be a multiple of 8 to prevent false sharing. Using N = "
+//                  << N << " instead." << '\n';
+//    }
 
-    Diffusion2D system(D, N, M, dt);
-    system.write_density("Solutions/RW_000.dat");
-
-    value_type tmax;
+    value_type t_max;
+    size_type n_runs;
 
     if (argc > 5) {
-        tmax = std::stod(argv[5]);
+        n_runs = std::stoul(argv[5]);
     } else {
-        tmax = 0.1;
+        n_runs = 1;
     }
 
+    if (argc > 6) {
+        t_max = std::stoul(argv[6]) * dt;
+    } else {
+        t_max = 0.1;
+    }
+
+    std::cout << "Running RW Scalar Simulations" << '\n';
+    std::cout << "N = " << N << '\t' << "dt = " << dt << '\t'
+              << "M = " << M << std::endl;
+
+    myInt64 min_cycles = 0;
+    value_type e_rms, final_time;
+
+    for(size_type i = 0; i < n_runs; i++) {
+        Diffusion2D system(D, N, M, dt);
+
 #ifdef USE_TIMER
-    timer t;
-    t.start();
+        timer t;
+        t.start();
 #endif // USE_TIMER
 
 #ifdef USE_TSC
-    myInt64 start, cycles;
-    start = start_tsc();
+        myInt64 start, cycles;
+        start = start_tsc();
 #endif // USE_TSC
-
-    while (system.time() < tmax) {
-        system.advance();
-    }
+        
+        while ( system.time() < t_max ) {
+            system.advance();
+        }
 
 #ifdef USE_TIMER
-    t.stop();
-    std::cout << "Timing: " << N << " " << t.get_timing() << std::endl;
+        t.stop();
+//        std::cout << "Timing: " << N << " " << t.get_timing() << std::endl;
 #endif // USE_TIMER
 
 #ifdef USE_TSC
-    cycles = stop_tsc(start);
-    std::cout << "Cycles = " << cycles << std::endl;
+        cycles = stop_tsc(start);
+//        std::cout << "Cycles = " << cycles << std::endl;
 #endif // USE_TSC
 
-    std::cout << "CFL # = " << system.CFL() << std::endl;
+        system.compute_rms_error();
+        e_rms = system.rms_error();
 
-    system.write_density("Solutions/RW_serial.dat");
-    system.write_reference("Solutions/RW_ref.dat");
+        if ( (e_rms < 0.001) && ( (cycles < min_cycles) || (min_cycles == 0) ) ) {
+            min_cycles = cycles;
+        }
 
-    std::cout << "RMS Error = " << system.rms_error() << std::endl;
+        if ( i == n_runs-1 ) {
+            final_time = system.time();
+            system.write_density("Solutions/RW_scalar.dat");
+            system.write_reference("Solutions/RW_ref.dat");
+        }
+    }
+
+    std::cout << "RMS Error of final run = " << e_rms << '\n';
+    std::cout << "At a final time = " << final_time << '\n';
+    std::cout << "Minimum Cycles over " << n_runs << " runs = " << min_cycles << '\n';
+
+    t_total.stop();
+    double timing = t_total.get_timing();
+//    std::cout << "Total program execution time = " << timing << " seconds\n" << std::endl;
+
+    unsigned hours = 0, minutes = 0;
+    if ( timing >= 3600 ) {
+        hours = static_cast<unsigned>(floor(timing/3600));
+        timing -= hours*3600;
+    }
+    if ( timing >= 60 ) {
+        minutes = static_cast<unsigned>(floor(timing/60));
+        timing -= minutes*60;
+    }
+    std::cout << "Total program execution time = " << hours << "h : " << minutes
+              << "m : " << timing << "s\n" << std::endl;
 
     return 0;
 }
+
