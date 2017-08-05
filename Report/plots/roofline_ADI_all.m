e30553923b2fbb422b655c2d85127770f2d6651f
Sam Maloney <56830868+sam-maloney@users.noreply.github.com>
2017-08-06 00:27:48 +0200

    finished ADI roofline, worked on report background

--- Report/plots/roofline_ADI_all.m
+++ Report/plots/roofline_ADI_all.m
@@ -5,7 +5,7 @@ bottom = -2;
 top = 5;
 
 % memory_bandwidth = 68; % GB/s
-memory_bandwidth = 11+14.5; % GB/s
+memory_bandwidth = (11000+14500)/2^10; % GB/s
 clock_speed = 2.6; % GHz
 scalar_peak = 4;
 vector_peak = 4*scalar_peak;
@@ -14,12 +14,12 @@ vector_peak = 4*scalar_peak;
 betaRAM = memory_bandwidth*2^30/(clock_speed*10^9);
 
 % betas for caches
-% betaL3 = 64/6.3 + 64/8.4;
-% betaL2 = 64/6.1 + 64/2.2;
+betaL3 = 64/6.3 + 64/8.4;
+betaL2 = 64/6.1 + 64/2.2;
 % betaL1 = 64/0.5;
 % betaL3 = 4*8;
-betaL3 = 64/6.1 + 64/2.2;
-betaL2 = 8*8;
+% betaL3 = 64/6.1 + 64/2.2;
+% betaL2 = 8*8;
 betaL1 = 12*8;
 
 I = [2^left 2^right];
@@ -30,34 +30,34 @@ ML1  = betaL1*I;
 pi_s = [scalar_peak scalar_peak];
 pi_v = [vector_peak vector_peak];
 
-N = [10 18 34 66 122 242 482 962 1922 3842];
+N = [10 18 34 66 122 242 482 962 1922 3842 7682];
 
 perf_AVX  = [1.925 2.064 2.310 2.045 1.958...
-             1.795 1.746 1.252 0.928 0.819];
-I_L2_AVX  = [2.023 7.287 11.219 0.050 0.028...
-             0.019 0.019 0.017 0.016 0.015];
-I_L3_AVX  = [4.386 13.860 49.258 146.745 0.411...
-             0.068 0.063 0.036 0.027 0.025];
-I_RAM_AVX = [17.762 62.560 256.690 1007.828 3582.589...
-             13416.279 26625.866 216.532 0.119 0.097];
+             1.795 1.746 1.252 0.928 0.819 0.801];
+I_L2_AVX  = [20.229 72.867 112.189 0.499 0.280...
+             0.193 0.185 0.174 0.165 0.151 0.140];
+I_L3_AVX  = [43.860 138.595 492.583 1467.446 4.109...
+             0.681 0.631 0.363 0.274 0.246 0.220];
+I_RAM_AVX = [177.625 625.597 2566.898 10078.278 35825.893...
+             134162.791 266258.661 2165.317 1.192 0.974 0.963];
          
 perf_scalar  = [1.549 1.720 1.693 1.670 1.625...
-                1.417 1.315 0.968 0.669 0.640];
-I_L2_scalar  = [1.960 6.343 2.667 0.043 0.024...
-                0.016 0.015 0.014 0.014 0.012];
-I_L3_scalar  = [3.974 14.131 48.339 117.831 0.914...
-                0.058 0.039 0.027 0.022 0.020];
-I_RAM_scalar = [15.998 57.100 223.206 882.583 2979.500...
-                11998.574 41484.375 249.248 0.104 0.084];
+                1.417 1.315 0.968 0.669 0.640 0.638];
+I_L2_scalar  = [19.604 63.431 26.669 0.431 0.244...
+                0.159 0.151 0.143 0.137 0.125 0.111];
+I_L3_scalar  = [39.736 141.312 483.390 1178.315 9.138...
+                0.579 0.389 0.275 0.221 0.200 0.169];
+I_RAM_scalar = [159.978 571.003 2232.055 8825.832 29795.005...
+                119985.741 414843.750 2492.483 1.036 0.837 0.842];
          
 perf_serial  = [0.734 0.526 0.480 0.459 0.449...
-                0.443 0.441 0.374 0.333 0.305];
+                0.443 0.441 0.374 0.333 0.305 0.274];
 I_L2_serial  = [38.730 139.276 283.299 1.017 0.797...
-                0.113 0.139 0.111 0.095 0.075];
+                0.113 0.139 0.111 0.095 0.075 0.084];
 I_L3_serial  = [77.529 319.366 1040.724 3515.800 173.098...
-                1.444 0.379 0.629 0.132 0.111];
+                1.444 0.379 0.629 0.132 0.111 0.118];
 I_RAM_serial = [324.468 1279.427 5348.837 20733.198 73016.277...
-                188433.814 989767.842 16011.485 2.335 1.913];
+                188433.814 989767.842 16011.485 2.335 1.913 1.941];
 
 hold on;
 
@@ -114,17 +114,31 @@ plot(log2(I),log2(ML3), '-b');
 plot(log2(I),log2(ML2), '-g');
 plot(log2(I),log2(ML1), '-k');
 
-plot(log2(I_RAM_AVX),log2(perf_AVX),'-g.');
-plot(log2(I_RAM_scalar),log2(perf_scalar),'-b.');
-plot(log2(I_RAM_serial),log2(perf_serial),'-r.');
+fac = 1/4;
 
-plot(log2(I_L3_AVX),log2(perf_AVX),'-g.');
-plot(log2(I_L3_scalar),log2(perf_scalar),'-b.');
-plot(log2(I_L3_serial),log2(perf_serial),'-r.');
+plot(log2(fac*I_RAM_AVX),log2(perf_AVX),'-g.');
+plot(log2(fac*I_RAM_scalar),log2(perf_scalar),'-b.');
+plot(log2(fac*I_RAM_serial),log2(perf_serial),'-r.');
 
-plot(log2(I_L2_AVX),log2(perf_AVX),'-g.');
-plot(log2(I_L2_scalar),log2(perf_scalar),'-b.');
-plot(log2(I_L2_serial),log2(perf_serial),'-r.');
+plot(log2(fac*I_L3_AVX),log2(perf_AVX),'-g.');
+plot(log2(fac*I_L3_scalar),log2(perf_scalar),'-b.');
+plot(log2(fac*I_L3_serial),log2(perf_serial),'-r.');
+
+plot(log2(fac*I_L2_AVX),log2(perf_AVX),'-g.');
+plot(log2(fac*I_L2_scalar),log2(perf_scalar),'-b.');
+plot(log2(fac*I_L2_serial),log2(perf_serial),'-r.');
+
+% plot(log2(I_RAM_AVX),log2(perf_AVX),'-g.');
+% plot(log2(I_RAM_scalar),log2(perf_scalar),'-b.');
+% plot(log2(I_RAM_serial),log2(perf_serial),'-r.');
+% 
+% plot(log2(I_L3_AVX),log2(perf_AVX),'-g.');
+% plot(log2(I_L3_scalar),log2(perf_scalar),'-b.');
+% plot(log2(I_L3_serial),log2(perf_serial),'-r.');
+% 
+% plot(log2(I_L2_AVX),log2(perf_AVX),'-g.');
+% plot(log2(I_L2_scalar),log2(perf_scalar),'-b.');
+% plot(log2(I_L2_serial),log2(perf_serial),'-r.');
 
 % h = text(log2(0.125),log2(4.4),'L3 Bandwidth');
 % set(h, 'rotation', 57);
